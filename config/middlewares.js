a963a2530b19ad94307126398406d46c4d34e212
Hung <hungqto@yahoo.com>
2023-09-04 03:17:55 -0700

    updated drivers form

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,30 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          "img-src": ["https:", "http:"],
+        },
+      },
+    },
+  },
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      header: "*",
+      origin: ["http://localhost:1337", "http://localhost:3000"],
+    },
+  },
+  // "strapi::security",
+  // "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
--- config/plugins.js
+++ config/plugins.js
@@ -18,25 +18,34 @@ module.exports = ({ env }) => ({
   },
   upload: {
     config: {
-      provider: "aws-s3",
       providerOptions: {
-        accessKeyId: env("AWS_ACCESS_KEY_ID"),
-        secretAccessKey: env("AWS_ACCESS_SECRET"),
-        region: env("AWS_REGION"),
-        params: {
-          Bucket: env("AWS_BUCKET_NAME"),
-        },
-      },
-      // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
-      actionOptions: {
-        upload: {
-          ACL: null,
-        },
-        uploadStream: {
-          ACL: null,
+        localServer: {
+          maxage: 300000,
         },
       },
     },
   },
+  // upload: {
+  //   config: {
+  //     provider: "aws-s3",
+  //     providerOptions: {
+  //       accessKeyId: env("AWS_ACCESS_KEY_ID"),
+  //       secretAccessKey: env("AWS_ACCESS_SECRET"),
+  //       region: env("AWS_REGION"),
+  //       params: {
+  //         Bucket: env("AWS_BUCKET_NAME"),
+  //       },
+  //     },
+  //     // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
+  //     actionOptions: {
+  //       upload: {
+  //         ACL: null,
+  //       },
+  //       uploadStream: {
+  //         ACL: null,
+  //       },
+  //     },
+  //   },
+  // },
   // ...
 });
