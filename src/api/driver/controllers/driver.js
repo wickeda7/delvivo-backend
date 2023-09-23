ae1bfe8951335c4beb2608f3412aabf27aa79fe1
Hung <hungqto@yahoo.com>
2023-09-23 07:12:04 -0700

    added login for drivers

--- src/api/driver/controllers/driver.js
+++ src/api/driver/controllers/driver.js
@@ -1,9 +1,25 @@
-'use strict';
+"use strict";
 
 /**
  * driver controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::driver.driver');
+module.exports = createCoreController("api::driver.driver", ({ strapi }) => ({
+  postDriver: async (ctx, next) => {
+    try {
+      const data = await strapi
+        .service("api::driver.driver")
+        .postDriver(ctx, next);
+      ctx.send(
+        {
+          data,
+        },
+        200
+      );
+    } catch (error) {
+      ctx.badRequest(error);
+    }
+  },
+}));
--- src/api/driver/services/driver.js
+++ src/api/driver/services/driver.js
@@ -1,9 +1,121 @@
-'use strict';
-
+"use strict";
+const {
+  getService,
+} = require("@strapi/plugin-users-permissions/server/utils/index");
+const _ = require("lodash");
+const utils = require("@strapi/utils");
+const { ApplicationError } = utils.errors;
 /**
  * driver service
  */
 
-const { createCoreService } = require('@strapi/strapi').factories;
+const { createCoreService } = require("@strapi/strapi").factories;
 
-module.exports = createCoreService('api::driver.driver');
+module.exports = createCoreService("api::driver.driver", ({ strapi }) => ({
+  postDriver: async (ctx, next) => {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      merchant_id,
+      phone,
+      address,
+      city,
+      state,
+      zip,
+      dl,
+      make,
+      model,
+      year,
+      color,
+      plate,
+      available,
+      profileImg,
+      carImg,
+    } = ctx.request.body.data;
+    const params = {
+      ..._.omit(
+        {
+          firstName: firstName,
+          lastName,
+          email,
+          password,
+          phoneNumber: phone,
+          merchant_id,
+          address,
+          city,
+          state,
+          zip,
+        },
+        [
+          "confirmed",
+          "blocked",
+          "confirmationToken",
+          "resetPasswordToken",
+          "provider",
+          "id",
+          "createdAt",
+          "updatedAt",
+          "createdBy",
+          "updatedBy",
+          "role",
+        ]
+      ),
+      provider: "local",
+    };
+
+    const { provider } = params;
+    const identifierFilter = {
+      $or: [{ email: email.toLowerCase() }],
+    };
+    const conflictingUserCount = await strapi
+      .query("plugin::users-permissions.user")
+      .count({
+        where: { ...identifierFilter, provider },
+      });
+    if (conflictingUserCount > 0) {
+      throw new ApplicationError("Email is already taken");
+    }
+    const newUser = {
+      ...params,
+      role: 4,
+      email: email.toLowerCase(),
+      confirmed: true,
+    };
+    const user = await getService("user").add(newUser);
+    if (user) {
+      const connect = { id: user.id };
+      const data = {
+        firstName,
+        lastName,
+        email,
+        password,
+        merchant_id,
+        phone,
+        address,
+        city,
+        state,
+        zip,
+        dl,
+        make,
+        model,
+        year,
+        color,
+        plate,
+        available,
+        profileImg,
+        carImg,
+        user: connect,
+      };
+      try {
+        const entry = await strapi.db
+          .query("api::driver.driver")
+          .create({ data: data });
+        return entry;
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    }
+  },
+}));
