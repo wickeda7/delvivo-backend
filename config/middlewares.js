363d6bb2a834b162edce7a41aace96c018229ed8
Hung <hungqto@yahoo.com>
2023-11-28 11:39:17 -0800

    fix CORS

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,15 @@
 module.exports = [
   "strapi::errors",
   "strapi::security",
-  "strapi::cors",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["*"],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+      keepHeaderOnError: true,
+    },
+  },
   "strapi::poweredBy",
   "strapi::logger",
   "strapi::query",
