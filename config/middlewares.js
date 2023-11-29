676c18c6f0cb31fed5d2c09d6ed5e3d482dfb078
Hung <hungqto@yahoo.com>
2023-11-29 04:53:40 -0800

    no message

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,17 +1,8 @@
 module.exports = [
   "strapi::errors",
   "strapi::security",
-
+  "strapi::cors",
   "strapi::poweredBy",
-  {
-    name: "strapi::cors",
-    config: {
-      origin: ["*"],
-      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
-      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
-      keepHeaderOnError: true,
-    },
-  },
   "strapi::logger",
   "strapi::query",
   "strapi::body",
