dc53e416e5e2f35db38c07d4294b4ccec6b76f80
Hung <hungqto@yahoo.com>
2023-12-26 15:57:02 -0800

    added Amazon S3

--- config/plugins.js
+++ config/plugins.js
@@ -29,15 +29,57 @@ module.exports = ({ env }) => ({
       },
     },
   },
+
   upload: {
     config: {
+      provider: "aws-s3",
       providerOptions: {
-        localServer: {
-          maxage: 300000,
+        s3Options: {
+          credentials: {
+            accessKeyId: env("AWS_ACCESS_KEY_ID"),
+            secretAccessKey: env("AWS_ACCESS_SECRET"),
+          },
+
+          region: env("AWS_REGION"),
+          params: {
+            ACL: env("AWS_ACL", "public-read"),
+            signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
+            Bucket: env("AWS_BUCKET"),
+          },
         },
       },
     },
   },
+
+  // upload: {
+  //   config: {
+  //     provider: "aws-s3",
+  //     providerOptions: {
+  //       accessKeyId: env("AWS_ACCESS_KEY_ID"),
+  //       secretAccessKey: env("AWS_ACCESS_SECRET"),
+  //       region: env("AWS_REGION"),
+  //       params: {
+  //         ACL: env("AWS_ACL", "public-read"),
+  //         signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
+  //         Bucket: env("AWS_BUCKET"),
+  //       },
+  //     },
+  //     actionOptions: {
+  //       upload: {},
+  //       uploadStream: {},
+  //       delete: {},
+  //     },
+  //   },
+  // },
+  // upload: {
+  //   config: {
+  //     providerOptions: {
+  //       localServer: {
+  //         maxage: 300000,
+  //       },
+  //     },
+  //   },
+  // },
   // upload: {
   //   config: {
   //     provider: "aws-s3",
